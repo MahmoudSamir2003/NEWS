51ba0c042232997f67d37f65082111cc3d68b79a
MahmoudSamir2003 <mahmoudsamir212089@gmail.com>
2023-02-22 13:03:53 +0200

    first api

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ input.addEventListener("change", function () {
 });
 function displayPost() {
   let row = "";
-  for (let a = 0; a <= posts.length; a++) {
+  for (let a = 0; a <= posts?.length; a++) {
     row += `<div class="col-md-4">
                   <img src="${
                     posts[a]?.urlToImage
